fb9ab61237aaeda0151831142f4f9b6669ccab1d
elisinwonderlnd <elisamarr4@gmail.com>
2024-09-04 12:56:52 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+//main page backgrounds
+
+let mainBackgrounds = document.querySelectorAll(".background");
+
+let imageIndex = 0;
+
+function changeBackground() {
+    mainBackgrounds[imageIndex].classList.remove("showing");
+    imageIndex++;
+    if (imageIndex >= mainBackgrounds.length) {
+        imageIndex = 0;
+    }
+    mainBackgrounds[imageIndex].classList.add("showing");
+}
+
+setInterval(changeBackground, 8000);
+
+
+//article page scrolling title
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+}
+
+function addAnimation() {
+    scrollers.forEach(scroller => {
+        scroller.setAttribute("data-animated", true);
+
+        const scrollerInner = scroller.querySelector(".scroller__inner");
+        const scrollerContent = Array.from(scrollerInner.children);
+        
+        scrollerContent.forEach((item) => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute("aria-hidden", true);
+            scrollerInner.appendChild(duplicatedItem);
+        });
+    });
+}
+
+// Back to Top Button
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
